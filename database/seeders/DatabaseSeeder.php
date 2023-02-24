a293be6d9f13a036aca28368a06253f77c40a03f
AiFx4Intel <89227642+AiFxApp@users.noreply.github.com>
2023-02-24 06:49:05 +0200

    Upgrade from Lara  v9 - 230223 AiFxApp

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,30 +19,30 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\Location::factory(10)->create();
-        \App\Models\Property::factory(50)->create();
-        \App\Models\Media::factory(500)->create();
-        \App\Models\PropertyEnquire::factory(50)->create();
+        // \App\Models\Location::factory(10)->create();
+        // \App\Models\Property::factory(50)->create();
+        // \App\Models\Media::factory(500)->create();
+        // \App\Models\PropertyEnquire::factory(50)->create();
 
         //-------- for static page ---------------
             $user = new User();
-            $user->name = 'Admin';
-            $user->email = 'admin@gmail.com';
+            $user->name = 'Tfx-Admin';
+            $user->email = 'tfx.webmaster@gmail.com';
             $user->email_verified_at = now();
-            $user->password = Hash::make('admin');
+            $user->password = Hash::make('Admin@2023');
             $user->remember_token = Str::random(10);
             $user->save();
 
             $page = new Page();
             $page->name = 'Contact Us';
             $page->slug = 'contact-us';
-            $page->content = 'Lorem ipsum dolor, sit amet llam quae rem cumque aliquam';
+            $page->content = 'ai.fx.intel@gmail.com';
             $page->save();
 
             $page = new Page();
             $page->name = 'About Us';
             $page->slug = 'about-us';
-            $page->content = 'Lorem ipsum dolor, sit amet rem cumque aliquam dolorem veritatis';
+            $page->content = 'Love developing with Laravel';
             $page->save();
         //------------ or ------------------
             // User::create([
---
+++ note-4-change-aifx230223.md
@@ -0,0 +1,14 @@
+# <a href="http://github.com/AiFxApp"> AiFx | Intel Brought To Life </a>
+#
+## Update: 
+#
+### Laravel:
+Current Laravel 8 -> Update to 9
+- Updated composer.json (as per laravel.com release) - 23.02.23
+
+# 
+## User:
+
+* add 3 layer user.. admin | seller | buyer (will work on this)
+with dahboard for each user level
+* add multiple language option 
